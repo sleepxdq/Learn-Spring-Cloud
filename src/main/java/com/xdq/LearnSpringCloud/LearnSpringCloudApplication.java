89ce0f2993b4e4354528a1063fca902408827612
dongqiang.xu <dongqiang.xu@boldseas.com>
2017-10-18 19:25:00 +0800

    create new project

---
+++ src/main/java/com/xdq/LearnSpringCloud/LearnSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package com.xdq.LearnSpringCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringCloudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xdq/LearnSpringCloud/LearnSpringCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xdq.LearnSpringCloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnSpringCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
